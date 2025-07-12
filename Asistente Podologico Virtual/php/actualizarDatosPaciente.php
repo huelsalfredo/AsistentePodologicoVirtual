66b23c7ed5b15a6369043b709518722ea119a816
huelsalfredo <165188956+huelsalfredo@users.noreply.github.com>
2025-07-12 15:09:40 -0300

    Add files via upload

--- Asistente Podologico Virtual/php/actualizarDatosPaciente.php
+++ Asistente Podologico Virtual/php/actualizarDatosPaciente.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-include '../conexion_be.php';
+include 'conexion_be.php';
 
 $idPaciente = $_SESSION['id_paciente'];
 
@@ -19,34 +19,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $stmt->bindParam(':id', $idPaciente);
 
     if ($stmt->execute()) {
-        header("Location: pantalla_paciente.php");
+        echo '<script src="../js/alerta.js"></script>;
+          <script>alerta("Datos actualizados con exito !!!", "pantalla_paciente.php");</script>';
     } else {
-        echo "<script>alert('Error al actualizar.'); window.location.href='pantalla_paciente.php';</script>";
-    }
+        echo '<script src="../js/alerta.js"></script>;
+          <script>alerta("Error al actualizar.", "pantalla_paciente.php");</script>';
+    }    
 }
-<?php
-
-session_start();
-include '../conexion_be.php';
-
-$idPaciente = $_SESSION['id_paciente'];
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $apellido = $_POST['apellido'];
-    $nombres = $_POST['nombres'];
-    $email = $_POST['email'];
-    $celular = $_POST['celular'];
-
-    $stmt = $conexion->prepare("UPDATE paciente SET apellido = :apellido, nombres = :nombres, email = :email, celular = :celular WHERE idPaciente = :id");
-    $stmt->bindParam(':apellido', $apellido);
-    $stmt->bindParam(':nombres', $nombres);
-    $stmt->bindParam(':email', $email);
-    $stmt->bindParam(':celular', $celular);
-    $stmt->bindParam(':id', $idPaciente);
-
-    if ($stmt->execute()) {
-        header("Location: pantalla_paciente.php");
-    } else {
-        echo "<script>alert('Error al actualizar.'); window.location.href='pantalla_paciente.php';</script>";
-    }
-}
+?>
