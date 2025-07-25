495c55cd93abf196a028f45a0d831a2831272a29
huelsalfredo <165188956+huelsalfredo@users.noreply.github.com>
2025-07-25 08:47:23 -0300

    Update nav_superior_paciente.php

--- Asistente Podologico Virtual/php/nav_superior_paciente.php
+++ Asistente Podologico Virtual/php/nav_superior_paciente.php
@@ -83,7 +83,7 @@
             <!-- Botón hamburguesa en móviles -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
                     aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>👨‍⚕️ 
+                <span class="navbar-toggler-icon"></span>
             </button>
 
             <!-- Menú desplegable -->
